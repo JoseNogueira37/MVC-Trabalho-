e0da87457719283ba85704b0473f0ae33d9ac63e
JoseNogueira37 <JoseNogueira.014.0@gmail.com>
2022-03-15 11:39:03 -0300

    Commit

---
+++ aula02/modelo/retorno.php
@@ -0,0 +1,37 @@
+<?php
+
+    $nome = $_REQUEST['nome'];
+    $nasc = $_REQUEST['nasc'];
+    $bebida = $_REQUEST['bebida'];
+
+    $date1 = new DateTime('now');
+    $date2 = new DateTime($nasc);
+
+    $i = $date1 -> diff($date2);
+    
+    if(empty($nome)){
+        
+        $dados = array(
+            "tipo" => 'error',
+            "mensagem" => 'Existe(m) campo(s) obrigatório(s) não preenchido(s).'
+        );
+        
+    } else {
+
+        if($i->y < 18) {
+            $dados = array(
+
+                "mensagem" => 'Olá, '.$nome.', sabemos que sua bebida favorita é '.$bebida.' e você não pode ingerir bebida alcoólica.'
+
+            );
+        }
+        else {
+            $dados = array(
+                "mensagem" => 'Olá, '.$nome.', sabemos que sua bebida favorita é '.$bebida.' e você pode ingerir bebida alcoólica.' 
+            );
+        }
+
+    }
+    
+
+    echo json_encode($dados);
